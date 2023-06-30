2351096e27b06d66b658ab2013232507b84f7749
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-06-30 11:55:10 +0100

    typeDefs.js file coded

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,6 +9,15 @@ const typeDefs = gql`
     savedBooks: [Book]
   }
 
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+  }
+  
 `;
 
 module.exports = typeDefs;
