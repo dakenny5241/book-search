1a771ab09574a033a7a18dbfa968f80ff359646a
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-07-03 12:29:38 +0100

    App.js file edited

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,17 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+
 function App() {
   return (
     <Router>
